d5a08556cc9dc080daa37e7ba32b0146e49342e1
nikniketa <nik.niketa@gmail.com>
2024-09-26 17:34:18 +0530

    Schema validatoion and data sanitizing

--- src/app.js
+++ src/app.js
@@ -2,22 +2,88 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const validator = require("validator");
 
+app.use(express.json());
 app.post("/signup", async (req, res) => {
-  const userData = new User({
-    firstName: "Niketa",
-    lastName: "Sikarwar",
-    emailId: "nik.niketa@gmail.com",
-    password: "Admin@123",
-  });
+  const data = req.body;
+  const userData = new User(data);
   try {
+    if (data?.skill.length > 10) {
+      throw new Error("UpdateOnly 10 skills allowed");
+    }
     await userData.save();
     res.send("User Added Successfully");
   } catch (err) {
-    res.status(400).send("Bad Request: ");
+    res.status(400).send("Bad Request: " + err.message);
   }
 });
-
+app.get("/user", async (req, res) => {
+  const data = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: data });
+    if (user.length === 0) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went Wrong");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send("no User found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+app.delete("/user", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.body.userId);
+    res.send("User Deleted");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+app.patch("/user/:userId", async (req, res) => {
+  const data = req.body;
+  try {
+    const forUpdate = [
+      "firstName",
+      "lastName",
+      "password",
+      "about",
+      "skill",
+      "photoURL",
+    ];
+    const isFieldUpdatable = Object.keys(data).every((x) =>
+      forUpdate.includes(x)
+    );
+    if (data?.skill.length > 10) {
+      throw new Error("UpdateOnly 10 skills allowed");
+    }
+    if (isFieldUpdatable) {
+      const user = await User.findByIdAndUpdate(
+        { _id: req.params.userId },
+        data,
+        {
+          runValidators: true,
+        }
+      );
+      res.send("User Update");
+    } else {
+      res.status(400).send("Update not allowed");
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong" + err.message);
+  }
+});
 connectDB()
   .then(() => {
     console.log("database successfully connected");
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,67 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minLength: 2,
+      maxLength: 25,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+      maxLength: 25,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      validate: function (val) {
+        return validator.isEmail(val);
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: function (val) {
+        return validator.isStrongPassword(val);
+      },
+    },
+    age: {
+      type: Number,
+      required: true,
+      trim: true,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: ["male", "female", "other"],
+    },
+    about: {
+      type: String,
+      default: "Hi, this is about myself",
+      trim: true,
+    },
+    skill: {
+      type: [String],
+    },
+    photoURL: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+      validate: function (val) {
+        return validator.isURL(val);
+      },
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const User = mongoose.model("User", userSchema);
 module.exports = User;
