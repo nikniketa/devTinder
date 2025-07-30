e612e5ae563814422ac0e4afd6fcdcc2ba5e9b23
nikniketa <nik.niketa@gmail.com>
2025-07-30 09:54:55 +0530

    Dark Theme for frontend

--- DevTinderWeb/devTinderWeb/README.md
+++ DevTinderWeb/devTinderWeb/README.md
@@ -3,3 +3,18 @@ Remove unnecessary code and create Hello World app
 Install Tailwind.css
 Install Daisy UI
 Add Navbar component to App.js
+create a Navbar.jsx seperate component file
+install react router dom
+Create the BrowserRouter>Routes > Route=/Body >RouteChildren
+Create an Outlet in your Body Component
+Create a footer
+
+
+
+
+Body
+    Navbar
+    Route =/ =>Feed
+    Route = /login => Login
+    Route = /connections =>Connection
+    Router= /profile => Profile
--- DevTinderWeb/devTinderWeb/src/Login.jsx
+++ DevTinderWeb/devTinderWeb/src/Login.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="flex justify-center mt-8">
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          className="input w-full"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          className="input w-full"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4 w-full">Login</button>
+      </fieldset>
+    </div>
+  );
 };
 
 export default Login;
